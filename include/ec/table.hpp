210bd84de150272e3ed2f3b7dd314c41b6ef5e1b
Demiralp <demiralpali@gmail.com>
2018-01-22 08:51:26 +0100

    Table progress.

--- include/ec/table.hpp
+++ include/ec/table.hpp
@@ -9,25 +9,39 @@
 
 namespace ec
 {
-template<typename... component_types>
+template <typename... component_types>
 class table
 {
 public:
   struct entity
   {
-    std::size_t                             id              ;
+    std::size_t                             id;
     std::bitset<sizeof...(component_types)> component_bitset;
   };
 
-  template<typename... argument_types>
-  void add_row   (argument_types&&... arguments)
+  template <typename... argument_types>
+  void add_entity   (argument_types&&... arguments)
   {
-    entities_.emplace(next_id_++, {});
+    return entities_.emplace({next_id_++, 0}, {}).first;
   }
-  void remove_row()
+  void remove_entity()
   {
     
   }
+  void get_entity   ()
+  {
+    
+  }
+
+  /*
+  - Insert or remove an entity (row).
+  - Insert or remove a component to or from an entity.
+  - Iterate through all entities (rows).
+  - Iterate through all entities (rows) containing one or more component types (columns).
+  - Query an entity's components        (select row  , given row index).
+  - Query an entity's component by type (select entry, given row index and column type).
+  - Query a component's entity          (select row  , given entry).
+  */
 
 protected:
   std::size_t                                                                    next_id_ = 0;
