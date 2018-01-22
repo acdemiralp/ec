eaa8361d98708acdcbf5521e272272579642b86b
demiralp <demiralp@vr.rwth-aachen.de>
2018-01-22 18:19:48 +0100

    Table progress.

--- include/ec/table.hpp
+++ include/ec/table.hpp
@@ -1,11 +1,11 @@
 #ifndef EC_TABLE_HPP_
 #define EC_TABLE_HPP_
 
-#include <bitset>
 #include <cstddef>
 #include <optional>
-#include <tuple>
 #include <unordered_map>
+
+#include <ec/entity.hpp>
 
 namespace ec
 {
@@ -13,26 +13,6 @@ template <typename... component_types>
 class table
 {
 public:
-  struct entity
-  {
-    std::size_t                             id;
-    std::bitset<sizeof...(component_types)> component_bitset;
-  };
-
-  template <typename... argument_types>
-  void add_entity   (argument_types&&... arguments)
-  {
-    return entities_.emplace({next_id_++, 0}, {}).first;
-  }
-  void remove_entity()
-  {
-    
-  }
-  void get_entity   ()
-  {
-    
-  }
-
   /*
   - Insert or remove an entity (row).
   - Insert or remove a component to or from an entity.
