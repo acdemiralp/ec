a6b4fc3a4a8227ff9d595f67bc30a20c68a2c090
demiralp <demiralp@vr.rwth-aachen.de>
2018-01-24 17:28:30 +0100

    Table refactoring.

--- include/ec/table.hpp
+++ include/ec/table.hpp
@@ -1,9 +1,12 @@
 #ifndef EC_TABLE_HPP_
 #define EC_TABLE_HPP_
 
+#include <algorithm>
 #include <optional>
+#include <tuple>
 #include <unordered_map>
 #include <utility>
+#include <vector>
 
 #include <boost/mp11.hpp>
 
@@ -15,22 +18,22 @@ template <typename... types>
 class table
 {
 public:
+  using entity            = entity<table<types...>>;
   using component_types   = boost::mp11::mp_list<types...>;
   using component_count   = boost::mp11::mp_size<component_types>;
   using component_storage = std::tuple<std::optional<types>...>;
-  using entity_type       = entity<table<types...>>;
 
-  entity_type              create_entity()
+  entity              create_entity()
   {
-    return entities_.emplace(entity_type(this), component_storage{1, std::nullopt, std::nullopt, std::nullopt}).first->first;
+    return entities_.emplace(entity(this), component_storage()).first->first;
   }
-  void                     delete_entity(const entity_type& entity)
+  void                delete_entity(const entity& entity)
   {
     entities_.erase(entity);
   }
-  std::vector<entity_type> entities     () const
+  std::vector<entity> entities     () const
   {
-    std::vector<entity_type> entities;
+    std::vector<entity> entities;
     std::transform(entities_.begin(), entities_.end(), std::back_inserter(entities), [ ] (const auto& iteratee)
     {
       return iteratee.first;
@@ -38,18 +41,18 @@ public:
     return entities;
   }
   template<typename... required_types>
-  std::vector<entity_type> entities     () const
+  std::vector<entity> entities     () const
   {
     using required_component_types   = boost::mp11::mp_list<required_types...>;
     using required_component_size    = boost::mp11::mp_size<required_component_types>;
     using required_component_indices = boost::mp11::mp_iota_c<required_component_size::value>;
 
-    std::vector<entity_type> entities;
+    std::vector<entity> entities;
     std::for_each(entities_.begin(), entities_.end(), [&entities] (const auto& iteratee)
     {
       auto valid = true;
 
-      boost::mp11::mp_for_each<required_component_indices>([&] (auto index)
+      boost::mp11::mp_for_each<required_component_indices>([&iteratee, &valid] (const auto index)
       {
         using component_index = boost::mp11::mp_find<component_types, boost::mp11::mp_nth_element_c<required_component_types, index, std::less>>;
         if (!iteratee.first.components_bitset()[component_index::value])
@@ -62,20 +65,8 @@ public:
     return entities;
   }
 
-  /*
-  - Entity
-    - Has Component  (entity)
-    - Has Components (entity)
-    - Get Scene      (entity)
-                     
-  - Component        
-    - Create         (entity)
-    - Delete         (entity)
-    - Get            (entity)
-  */
-
 protected:
-  std::unordered_map<entity_type, component_storage> entities_;
+  std::unordered_map<entity, component_storage> entities_;
 };
 }
 
